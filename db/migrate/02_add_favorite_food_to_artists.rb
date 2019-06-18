286663d76ce6b997839043cb2a5e47114a5fc3ce
Kari O'Neal <triadfirewood@gmail.com>
2019-06-18 12:50:21 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,4 +1,4 @@
-class AddFavoriteFoodToArtist < ActiveRecord::Migration
+class AddFavoriteFoodToArtist < ActiveRecord::Migration[5.1]
     
     def change 
       add_column :artists, :favorite_food, :string
